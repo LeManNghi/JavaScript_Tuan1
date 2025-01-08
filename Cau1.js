9054b5ed1578a94358723793ea8150790439e842
Le Man Nghi <22652921.nghi@student.iuh.edu.vn>
2025-01-08 19:24:33 +0700

    Create JSF Part1 Question 1

---
+++ Cau1.js
@@ -0,0 +1,21 @@
+let mass = 78;
+let height = 1.69;
+BMI = mass/(height**2);
+console.log("Data 1")
+console.log("Mark's BMI"+BMI)
+
+mass = 92;
+height = 1.95;
+BMI = mass/(height**2);
+console.log("John's BMI"+BMI)
+
+console.log("Data 2")
+mass = 95;
+height = 1.88;
+BMI = mass/(height**2);
+console.log("Mark's BMI"+BMI)
+
+mass = 85;
+height = 1.76;
+BMI = mass/(height**2);
+console.log("John's BMI"+BMI)
